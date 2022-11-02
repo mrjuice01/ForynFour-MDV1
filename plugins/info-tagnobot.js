ac683f62c647b1eac3b50e6ed6a08f6c95ae14c2
Xiaomi Team <112295130+XiaomiBOT@users.noreply.github.com>
2022-11-02 08:18:35 +0700

    Update info-tagnobot.js

--- plugins/info-tagnobot.js
+++ plugins/info-tagnobot.js
@@ -3,10 +3,10 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, text }) => {
 let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 let krizyn = 'https://telegra.ph/file/65289acb075702f9bb306.jpg'
-await conn.send3ButtonImg(m.chat, krizyn, "=====『 TAG TERDETEKSI 』=====\n\nIya ada perlu apa dengan bot? ", '📮 Silahkan Pilih Button dibawah ini', 'Group', '.allgc', 'Menu', '.menu', 'Store', '.store', m)
+await conn.send3ButtonImg(m.chat, hanz, "=====『 TAG TERDETEKSI 』=====\n\nIya ada perlu apa dengan bot? ", '📮 Silahkan Pilih Button dibawah ini', 'Group', '.allgc', 'Menu', '.menu', 'Store', '.store', m)
                         
 }
-handler.customPrefix = /(@+6289526416801)/i
+handler.customPrefix = /(@+62857791505013)/i
 handler.command = new RegExp
 
 module.exports = handler
